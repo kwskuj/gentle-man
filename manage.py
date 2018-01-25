f9779936f2e294e07d37a0bdf56d523bb2435e08
kawasaki <kwskuj55@icloud.com>
2018-01-25 21:48:26 +0900

    検索箇所css高さ修正

--- manage.py
+++ manage.py
@@ -20,10 +20,5 @@ def render_allvideo():
 def ajax_test():
     return json.dumps({'1':[aaaaaa],'2':[bbbbbbbb],'3':[ccccc]})
 
-# @app.route("/test")
-# def db_test():
-#     connnection = psycopg2.connnect("host=localhost port=5432 dbname=postgres" user=kawsakiyuji)
-#     connnection.get_backend_pid()
-
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
