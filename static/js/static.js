b7fb8f5fa23656de173dc4b2d7bd6f6e9444048b
kawasaki <kwskuj55@icloud.com>
2018-02-01 21:08:49 +0900

    TOP一段目動的生成
    画像データベースから持ってきてるけど、一枚しか出ない

--- static/js/static.js
+++ static/js/static.js
@@ -4,8 +4,7 @@ $(function () {
   right();
   display_control("contents_wrap", true);
   menu_button();
-  test02();
-  create_tag()
+  url();
 });
 /**
  * ディスプレイコントロール
@@ -17,96 +16,94 @@ function display_control(id, is_disp) {
   $('#' + id).css('display', disp);
 }
 
-function test() {
-  var test_data;
+/**
+ * ここでサーバーサイドにアクセスして
+ * データベースから持ってきた画像を変数に格納して
+ * タグに入れ込んで表出させてる
+ * aタグはめんどかったからそのまま
+ */
+function url() {
   $.ajax({
     type: "GET",
     url: "/test",
   }).done(function (json) {
-    test_data = JSON.parse(json);
-    console.log(test_data[0])
-    return test_data;
+    var test_data = JSON.parse(json);
+    test_data.forEach(function (val,idx) {
+      var data = val[1];
+      console.log(data)
+      var li_tag = $("<li>", {
+        id: "li_" + idx
+      });
+      var img = $("<img src=" + "'"+ data+"'" + ">")
+      var a_tag = $("<a target=_blank class='' href ='https://www.xvideos.com/embedframe/15057399'><img src='https://static-hw.xvideos.com/v3/img/player/icon-play.svg'></a>")
+      li_tag.append(a_tag,img);
+      $(".slideSampleThumbnail").append(li_tag);
+    });
   });
 }
 
-function test_2() {
-  test().forEach(function(val,idx) {
-    console.log(val)
-  });
-}
+function top_page_url() {
+      url();
+      console.log(test_data)
+    }
 
 /**
  * メニューボタンイベント
  */
 function menu_button() {
-  $(document).on('click', '#hunbergar', function () {
-    a = 1
-    $(this).toggleClass("on");
-    if ($(this).hasClass("on")) {
-      $('#main_container').css({
-        "width": "80%"
+      $(document).on('click', '#hunbergar', function () {
+        a = 1
+        $(this).toggleClass("on");
+        if ($(this).hasClass("on")) {
+          $('#main_container').css({
+            "width": "80%"
+          });
+          display_control('side_bar', true);
+        } else {
+          $('#main_container').css({
+            "width": "100%"
+          });
+          display_control('side_bar', false);
+        }
       });
-      display_control('side_bar', true);
-    } else {
-      $('#main_container').css({
-        "width": "100%"
-      });
-      display_control('side_bar', false);
     }
-  });
-}
 /**
  * スライダー左ボタン
  */
 function left() {
-  $(document).on('click', '.left_button', function () {
-    var clone = $(this).parent("div").children("ul").children("li:first").clone(true);
-    $(this).parent("div").children("ul").children("li:first").animate({
-      left: -70
-    }, 50000);
-    $(this).parent("div").children("ul").children("li:first").remove();
-    clone.clone(true).insertAfter($(this).parent("div").children("ul").children("li:last"));
-  });
-}
+      $(document).on('click', '.left_button', function () {
+        var clone = $(this).parent("div").children("ul").children("li:first").clone(true);
+        $(this).parent("div").children("ul").children("li:first").animate({
+          left: -70
+        }, 50000);
+        $(this).parent("div").children("ul").children("li:first").remove();
+        clone.clone(true).insertAfter($(this).parent("div").children("ul").children("li:last"));
+      });
+    }
 /**
  * スライダー右ボタン
  */
 function right() {
-  $(document).on('click', '.right_button', function () {
-    var clone = $(this).parent("div").children("ul").children("li:last").clone(true);
-    $(this).parent("div").children("ul").children("li:last").animate({
-      right: -70
-    }, 50000);
-    $(this).parent("div").children("ul").children("li:last").remove();
-    clone.clone(true).insertBefore($(this).parent("div").children("ul").children("li:first"));
-  });
-}
-
-function test02(){
-   var url = ['https://www.xvideos.com/embedframe/30973485','https://www.xvideos.com/embedframe/16490523','https://www.xvideos.com/embedframe/30042231','https://www.xvideos.com/embedframe/30190627','https://www.xvideos.com/embedframe/29464917','https://www.xvideos.com/embedframe/17596727'];
-   url.forEach(function(val,indx){
-     console.log (val);
-     $('all-video_container_item',{
-        id:'div' + '_' + indx
+      $(document).on('click', '.right_button', function () {
+        var clone = $(this).parent("div").children("ul").children("li:last").clone(true);
+        $(this).parent("div").children("ul").children("li:last").animate({
+          right: -70
+        }, 50000);
+        $(this).parent("div").children("ul").children("li:last").remove();
+        clone.clone(true).insertBefore($(this).parent("div").children("ul").children("li:first"));
       });
-     var a_tag = $('<a href=""><img src="https://static-hw.xvideos.com/v3/img/player/icon-play.svg"></a>',{
-       src:val,
-     });
-     $(".all-video_container_item").append(a_tag);
-   });
-}
+    }
 
-<<<<<<< HEAD
-function create_tag(){
-  var url = "https://www.xvideos.com/embedframe/30973485"; 
-=======
-var url = "https://www.xvideos.com/embedframe/30973485";
->>>>>>> 23838ef1ec0bb8b7c6645c9b68b50db92663bb6d
-  var a_href = "href ="+'"'+ url +'"';
-  console.log(a_href);
-  var a_tag = $("<a " + a_href + "></a>")
-  $("html").append(a_tag)
-<<<<<<< HEAD
-}
-=======
->>>>>>> 23838ef1ec0bb8b7c6645c9b68b50db92663bb6d
+function test02() {
+      var url = ['https://www.xvideos.com/embedframe/30973485', 'https://www.xvideos.com/embedframe/16490523', 'https://www.xvideos.com/embedframe/30042231', 'https://www.xvideos.com/embedframe/30190627', 'https://www.xvideos.com/embedframe/29464917', 'https://www.xvideos.com/embedframe/17596727'];
+      url.forEach(function (val, indx) {
+        console.log(val);
+        $('all-video_container_item', {
+          id: 'div' + '_' + indx
+        });
+        var a_tag = $('<a href=""><img src="https://static-hw.xvideos.com/v3/img/player/icon-play.svg"></a>', {
+          src: val,
+        });
+        $(".all-video_container_item").append(a_tag);
+      });
+    }
